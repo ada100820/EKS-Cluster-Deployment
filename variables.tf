032eb44a9ee7f4d1d1bc3ab7b59dce6b110b62a4
Adarsh Shukla <89727799+ada100820@users.noreply.github.com>
2025-04-03 03:57:39 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "test-cluster"
+}
+
+variable "cluster_version" {
+  type    = string
+  default = "1.25"
+}
+
+variable "desired_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "max_capacity" {
+  type    = number
+  default = 3
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.large"
+}
