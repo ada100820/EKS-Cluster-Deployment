258d2fee6cced94b758efbae86d68124897ac6fb
Adarsh Shukla <89727799+ada100820@users.noreply.github.com>
2025-04-03 03:52:46 -0700

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.9"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.13"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# The helm and kubernetes providers rely on data sources defined in main.tf
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
